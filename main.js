91f27c2b6b35e3b1c40522c325130c521c79e4f3
converge <joaopaulo.mailfs@gmail.com>
2016-12-12 23:26:31 -0200

    removed tab spaces

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 define(function (require, exports, module) {
 
-		"use strict";	
-		
-		require('./lib/nasm');
-		
-		var codemirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
-    codemirror.defineMIME("application/x-nasm", {name: "nasm", scriptingModeSpec:"nasm"});
-		
-    var LanguageManager = brackets.getModule("language/LanguageManager");
+  "use strict";	
 
-    LanguageManager.defineLanguage("nasm", {
-        name: "NASM Asssembler",
-        mode: "nasm",
-        fileExtensions: ["asm"]
-    });
-    
+  require('./lib/nasm');
+
+  var codemirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
+  codemirror.defineMIME("application/x-nasm", {name: "nasm", scriptingModeSpec:"nasm"});
+
+  var LanguageManager = brackets.getModule("language/LanguageManager");
+
+  LanguageManager.defineLanguage("nasm", {
+    name: "NASM Asssembler",
+    mode: "nasm",
+    fileExtensions: ["asm"]
+  });
+
 });
