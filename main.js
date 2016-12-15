54d4d504c71b7206c0c25cd760ab162b93e792b4
converge <joaopaulo.mailfs@gmail.com>
2016-12-15 00:07:57 -0200

    added screenshot example, converted my NASM Assembly CodeMirror mode into Brackets syntax highlighter module

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ define(function (require, exports, module) {
 
   "use strict";	
 
-  require('./lib/nasm');
+  require('./lib/nasm/nasm');
 
-  var codemirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
-  codemirror.defineMIME("application/x-nasm", {name: "nasm", scriptingModeSpec:"nasm"});
+  var CodeMirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
+  CodeMirror.defineMIME("application/x-nasm", {name: "nasm", scriptingModeSpec:"nasm"});
 
+	// define NASM language
   var LanguageManager = brackets.getModule("language/LanguageManager");
 
   LanguageManager.defineLanguage("nasm", {
